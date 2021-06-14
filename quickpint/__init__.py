bc27c146e7e15182b4a8d400ff3ad7cf2807118f
Alex Stapleton <alexs@prol.etari.at>
2021-06-14 20:27:48 +0100

    Patching and benchmarks

--- quickpint/__init__.py
+++ quickpint/__init__.py
@@ -1,11 +1,27 @@
+import pint.pint_eval
+import pint.registry
+import pint.util
+
 from . import quickpint as fast_quickpint
 
+_original_build_eval_tree = pint.pint_eval.build_eval_tree
 
-def build_eval_tree(tokens):
+def build_eval_tree(tokens, *args):
     if not isinstance(tokens, list):
         tokens = list(tokens)
-    
     return fast_quickpint.build_eval_tree(tokens)
+
+
+def patch():
+    pint.pint_eval.build_eval_tree = build_eval_tree
+    pint.registry.build_eval_tree = build_eval_tree
+    pint.util.build_eval_tree = build_eval_tree
+
+
+def unpatch():
+    pint.pint_eval.build_eval_tree = _original_build_eval_tree
+    pint.registry.build_eval_tree = _original_build_eval_tree
+    pint.util.build_eval_tree = _original_build_eval_tree
 
 
 __all__ = [
--- quickpint/test_quickpint.py
+++ quickpint/test_quickpint.py
@@ -1,9 +1,39 @@
-from tokenize import tokenize
-from io import BytesIO
-from quickpint import build_eval_tree
+import quickpint
+import pint
+import pytest
+import importlib
+
+from pint.compat import tokenizer
+
+@pytest.fixture
+def quick():
+    quickpint.patch()
+    yield
+    quickpint.unpatch()
+    importlib.reload(pint)
 
 
-def test_quickpint():
-    input = "gram / meter ** 2 / second"
-    tokens = tokenize(BytesIO(input.encode("utf-8")).readline)
-    assert build_eval_tree(tokens)
+def test_quickpint_fast(quick, benchmark):
+    registry = pint.UnitRegistry()
+
+    units = ("meter", "kilometer", "second", "minute", "angstrom")
+    expressions = [f"1.0 {unit} * 1 * 2 * 3 * 4 * 5" for unit in units]
+
+    def _test():
+        for expression in expressions:
+            assert registry.parse_expression(expression)
+
+    benchmark(_test)
+
+
+def test_quickpint_slow(benchmark):
+    registry = pint.UnitRegistry()
+
+    units = ("meter", "kilometer", "second", "minute", "angstrom")
+    expressions = [f"1.0 {unit} * 1 * 2 * 3 * 4 * 5" for unit in units]
+
+    def _test():
+        for expression in expressions:
+            assert registry.parse_expression(expression)
+
+    benchmark(_test)
