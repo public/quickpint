50948269975bcc709ebd6e23ea7682c524bb8bc7
Alex Stapleton <alexs@prol.etari.at>
2021-06-20 14:04:20 +0100

    patch the tokenizer

--- quickpint/__init__.py
+++ quickpint/__init__.py
@@ -1,10 +1,13 @@
 import pint.pint_eval
 import pint.registry
 import pint.util
+import pint.compat
 
 from . import quickpint as fast_quickpint
 
 _original_build_eval_tree = pint.pint_eval.build_eval_tree
+_original_tokenizer = pint.compat.tokenizer
+
 
 def build_eval_tree(tokens, *args):
     if not isinstance(tokens, list):
@@ -12,17 +15,27 @@ def build_eval_tree(tokens, *args):
     return fast_quickpint.build_eval_tree(tokens)
 
 
+tokenizer = fast_quickpint.tokenizer
+
+
 def patch():
+    pint.compat.tokenizer = tokenizer
+    pint.registry.tokenizer = tokenizer
+    pint.util.tokenizer = tokenizer
+
     pint.pint_eval.build_eval_tree = build_eval_tree
     pint.registry.build_eval_tree = build_eval_tree
     pint.util.build_eval_tree = build_eval_tree
 
 
 def unpatch():
+    pint.compat.tokenizer = _original_tokenizer
+    pint.registry.tokenizer = _original_tokenizer
+    pint.util.tokenizer = _original_tokenizer
+
     pint.pint_eval.build_eval_tree = _original_build_eval_tree
     pint.registry.build_eval_tree = _original_build_eval_tree
     pint.util.build_eval_tree = _original_build_eval_tree
 
-__all__ = [
-    "build_eval_tree"
-]
+
+__all__ = ["build_eval_tree"]
